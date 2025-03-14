d59dd7b381f277daa2ba2f09fe8ab49267aa2040
srv-23 <srvchourasia10@gmail.com>
2025-03-14 11:25:07 +0530

    project completed

---
+++ backend/lib/arcjet.js
@@ -0,0 +1,27 @@
+import arcjet, { tokenBucket, shield, detectBot, slidingWindow } from "@arcjet/node";
+
+import "dotenv/config";
+
+export const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  characteristics: ["ip.src"],
+  rules: [
+  
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE",
+
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+   
+      ],
+    }),
+   
+    tokenBucket({
+      mode: "LIVE",
+      refillRate: 30,
+      interval: 5,
+      capacity: 20,
+    }),
+  ],
+});
